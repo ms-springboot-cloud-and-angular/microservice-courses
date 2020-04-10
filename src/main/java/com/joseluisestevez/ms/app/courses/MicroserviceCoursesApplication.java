e964d7298c84757eddaa4b01eb39dc3206919bdd
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-10 17:46:47 +0200

    Add relationship with student

--- src/main/java/com/joseluisestevez/ms/app/courses/MicroserviceCoursesApplication.java
+++ src/main/java/com/joseluisestevez/ms/app/courses/MicroserviceCoursesApplication.java
@@ -2,10 +2,12 @@ package com.joseluisestevez.ms.app.courses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({ "com.joseluisestevez.ms.app.courses.models.entity", "com.joseluisestevez.ms.commons.students.models.entity" })
 public class MicroserviceCoursesApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/joseluisestevez/ms/app/courses/models/entity/Course.java
+++ src/main/java/com/joseluisestevez/ms/app/courses/models/entity/Course.java
@@ -1,16 +1,22 @@
 package com.joseluisestevez.ms.app.courses.models.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.joseluisestevez.ms.commons.students.models.entity.Student;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,10 +37,21 @@ public class Course {
     @Column(name = "create_at")
     private Date createAt;
 
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Student> students = new ArrayList<>();
+
     @PrePersist
     public void prePersist() {
         // You can use the @CreationTimestamp and @UpdateTimestamp attribute annotation
         this.createAt = new Date();
+    }
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        this.students.remove(student);
     }
 
 }
