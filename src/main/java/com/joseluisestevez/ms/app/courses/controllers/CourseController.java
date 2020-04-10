1a4292e75afbe93b215d2b8070837ced1967dabb
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-10 18:26:58 +0200

    Create rest service assign students and delete student

--- src/main/java/com/joseluisestevez/ms/app/courses/controllers/CourseController.java
+++ src/main/java/com/joseluisestevez/ms/app/courses/controllers/CourseController.java
@@ -1,5 +1,6 @@
 package com.joseluisestevez.ms.app.courses.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.joseluisestevez.ms.app.courses.models.entity.Course;
 import com.joseluisestevez.ms.app.courses.services.CourseService;
 import com.joseluisestevez.ms.commons.controllers.CommonController;
+import com.joseluisestevez.ms.commons.students.models.entity.Student;
 
 @RestController
 public class CourseController extends CommonController<Course, CourseService> {
@@ -29,4 +31,34 @@ public class CourseController extends CommonController<Course, CourseService> {
         Course courseSaved = service.save(currentCourse);
         return ResponseEntity.status(HttpStatus.CREATED).body(courseSaved);
     }
+
+    @PutMapping("/{id}/assign-students")
+    public ResponseEntity<?> assignStudents(@PathVariable Long id, @RequestBody List<Student> students) {
+        Optional<Course> optional = service.findById(id);
+        if (optional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Course currentCourse = optional.get();
+
+        students.forEach(s -> {
+            currentCourse.addStudent(s);
+        });
+
+        Course courseSaved = service.save(currentCourse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseSaved);
+    }
+
+    @PutMapping("/{id}/delete-student")
+    public ResponseEntity<?> deleteStudent(@PathVariable Long id, @RequestBody Student student) {
+        Optional<Course> optional = service.findById(id);
+        if (optional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Course currentCourse = optional.get();
+        currentCourse.removeStudent(student);
+
+        Course courseSaved = service.save(currentCourse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseSaved);
+    }
+
 }
