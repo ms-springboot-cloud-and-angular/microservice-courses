3239c34965a9bb1e8bbf98aff542201a2a0670ba
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-13 11:07:29 +0200

    Add pagination

--- src/main/java/com/joseluisestevez/ms/app/courses/models/repository/CourseRepository.java
+++ src/main/java/com/joseluisestevez/ms/app/courses/models/repository/CourseRepository.java
@@ -1,11 +1,11 @@
 package com.joseluisestevez.ms.app.courses.models.repository;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.joseluisestevez.ms.app.courses.models.entity.Course;
 
-public interface CourseRepository extends CrudRepository<Course, Long> {
+public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
 
     @Query("select c from Course c join fetch c.students s WHERE s.id = ?1")
     Course findCourseByStudentId(Long id);
